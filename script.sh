39205fb335c147e0a5fc8044ebe6693a5dcb2c6d
root <root@vps-1892ecc3.vps.ovh.net>
2021-04-12 14:20:03 +0000

    certbot

--- script.sh
+++ script.sh
@@ -4,7 +4,19 @@ echo "Installation paquets APT"
 apt update
 apt install -y mariadb-server mariadb-client
 apt install -y php apache2 libapache2-mod-php php-mysql php-xml
-apt install -y composer vim git
+apt install -y composer vim git snapd
+
+echo "Config snapd + certbot"
+snap install core
+snap refresh core
+snap install --classic certbot
+
+CERTBOT=$(ls /usr/bin | grep certbot)
+if [ -z "$CERTBOT" ]
+then
+  echo "On créé le lien /usr/bin/certbot"
+  ln -s /snap/bin/certbot /usr/bin/certbot
+fi
 
 MD5_DEST=$(md5sum /etc/apache2/sites-available/000-default.conf | awk '{print $1}')
 MD5_SRC=$(md5sum 000-default.conf | awk '{print $1}')
@@ -23,3 +35,4 @@ composer install
 chown -R www-data:www-data /var/www/html/
 source .env.dev
 
+echo "Penser à taper la commander certbot --apache"
