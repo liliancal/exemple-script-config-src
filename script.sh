b27bededb480f40380af0bc7761e46caf2548713
liliancal <43822749+liliancal@users.noreply.github.com>
2021-04-15 15:58:01 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -28,8 +28,16 @@ then
 	service apache2 restart
 fi
 
+CHECK_GIT_CONFIG=$(cd /var/www/html && git config --get remote.origin.url)
+if [ -z "$CHECK_GIT_CONFIG" ]
+then
+  echo "Mise en place config git"
+  cd /var/www/html && rm -rf .git/
+  git init
+  git remote add origin https://github.com/Whmn777/piscine.git
+fi
+
 echo "Pull sources git"
-cd /var/www/html
 git pull origin master
 composer install
 chown -R www-data:www-data /var/www/html/
